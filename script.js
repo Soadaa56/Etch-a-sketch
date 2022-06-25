ce4c81b8a3e53b17ef6a4eae6fff4f7366046f01
soadaa <Soadhlh@gmail.com>
2022-06-25 17:52:30 -0400

    function to change black on mouseover
    This function ceases to work after hitting reset, the class on the div won't change

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ function makeGrid(rows, cols) {
 makeGrid(4, 4); // default size of grid
 
 const reset = document.querySelector("body > div > div.sidebar > button");
+const cells = document.querySelectorAll('.grid-item');
 
 reset.addEventListener('click', () => {
-    let cells = document.querySelectorAll('.grid-item');
     cells.forEach(cell => {
         cell.remove();
     });
 
     let x = prompt('Input a number up to 100', '');
     makeGrid(x, x);
+    cells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
 });
 
+cells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
+
+function changeColor(e) {
+  console.log(e.target.className);
+  e.target.classList.remove('grid-item');  
+  e.target.classList.add('item-black');
+}
+
+// container.addEventListener('mouseenter', event   => {
+//     let target = event.target;
+//     console.log(target);
+// });
