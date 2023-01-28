01d8ea512553dbd49bdef4f7bb06dd3aac3c9e8a
soadaa <Soadhlh@gmail.com>
2023-01-28 13:16:48 -0500

    Input field for gridSize
    More user friendly to have an input field than to have a prompt. There is still an alert being used when a number is too large, which isn't user friendly either.

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const container = document.getElementById('container');
 
-function makeGrid(rows, cols) {
+function makeGrid(rows, columns) {
+    if (rows < 0) {
+        rows = 1;
+        columns = 1;
+        makeGrid(rows, columns);
+    }
     container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    container.style.setProperty('--grid-cols', columns);
+    for (c = 0; c < (rows * columns); c++) {
       let cell = document.createElement("div");
       cell.innerText = '';
       container.appendChild(cell).className = "grid-item";
       cell.addEventListener('mouseenter', changeColor);
     };
-  };
+};
   
 makeGrid(4, 4); // default size of grid
 gridSize = 4
 
 const changeSize = document.querySelector("#change-size");
+const sizeInput = document.getElementById('grid-size');
 
 changeSize.addEventListener('click', () => {
     const cells = document.querySelectorAll('.grid-item');
@@ -22,12 +28,11 @@ changeSize.addEventListener('click', () => {
         cell.remove();
     });
 
-    let x = prompt('Input a number up to 100', '');
-    if (x > 100) {
-      alert('Sorry, too big. Try a number below 100')
+    gridSize = sizeInput.value
+    if (gridSize > 99) {
+      alert('Sorry, too big. Try a number below 100. Gets too laggy!')
     } else {
-      makeGrid(x, x);
-      gridSize = x;
+      makeGrid(gridSize, gridSize);
     }
 });
 
@@ -43,6 +48,5 @@ reset.addEventListener('click', () => {
 });
 
 function changeColor(e) {
-  console.log(e.target.className); 
   e.target.classList.add('item-black');
 };
